a85dcace06d8d48e239ba77d9eecb58bfc8bdc73
Javier Dominguez II <Javi7057@gmail.com>
2022-07-31 16:50:51 -0500

    final commit before code refactoring

--- pages/Portfolio.vue
+++ pages/Portfolio.vue
@@ -2,24 +2,24 @@
   <div>
     <div class="bg-base-200 min-h-screen">
       <div class="pt-8"><h1 class="title">Portfolio</h1></div>
-      <div class="divider w-11/12 mx-auto"></div>
+      <div class="divider w-96 mx-auto"></div>
       <!-- flex container -->
       <div class="flex flex-wrap">
         <div class="card glass w-96 bg-base-100 shadow-xl mx-auto my-8">
           <figure class="px-10 pt-10">
-            <img src="/images/Arrive.png" alt="Shoes" class="rounded-xl" />
+            <img src="/images/Nuxtfolio.png" alt="Shoes" class="rounded-xl" />
           </figure>
           <div class="card-body items-center text-center">
             <h2 class="card-title text-xl font-bold text-secondary">Role</h2>
-            <p>Business Development Representative</p>
+            <p>Full Stack Developer</p>
             <h2 class="card-title text-xl font-bold text-secondary">
               Industry
             </h2>
-            <p>Logistics</p>
+            <p>Web Development</p>
             <h2 class="card-title text-xl font-bold text-secondary">
-              Start Date
+              Framework Used
             </h2>
-            <p>July 5th, 2022</p>
+            <p>Nuxt 3 with Typescript</p>
             <div class="card-actions mt-2">
               <a
                 href="https://www.arrivelogistics.com/"
@@ -37,21 +37,21 @@
             </div>
           </div>
         </div>
-        <div class="card glass w-96 bg-base-100 shadow-xl mx-auto my-8">
+        <!-- <div class="card glass w-96 bg-base-100 shadow-xl mx-auto my-8">
           <figure class="px-10 pt-10">
-            <img src="/images/Arrive.png" alt="Shoes" class="rounded-xl" />
+            <img src="/images/Nuxtfolio.png" alt="Shoes" class="rounded-xl" />
           </figure>
           <div class="card-body items-center text-center">
             <h2 class="card-title text-xl font-bold text-secondary">Role</h2>
-            <p>Business Development Representative</p>
+            <p>Full Stack Developer</p>
             <h2 class="card-title text-xl font-bold text-secondary">
               Industry
             </h2>
-            <p>Logistics</p>
+            <p>Web Development</p>
             <h2 class="card-title text-xl font-bold text-secondary">
-              Start Date
+              Framework Used
             </h2>
-            <p>July 5th, 2022</p>
+            <p>Nuxt 3 with Typescript</p>
             <div class="card-actions mt-2">
               <a
                 href="https://www.arrivelogistics.com/"
@@ -68,39 +68,7 @@
               ></a>
             </div>
           </div>
-        </div>
-        <div class="card glass w-96 bg-base-100 shadow-xl mx-auto my-8">
-          <figure class="px-10 pt-10">
-            <img src="/images/Arrive.png" alt="Shoes" class="rounded-xl" />
-          </figure>
-          <div class="card-body items-center text-center">
-            <h2 class="card-title text-xl font-bold text-secondary">Role</h2>
-            <p>Business Development Representative</p>
-            <h2 class="card-title text-xl font-bold text-secondary">
-              Industry
-            </h2>
-            <p>Logistics</p>
-            <h2 class="card-title text-xl font-bold text-secondary">
-              Start Date
-            </h2>
-            <p>July 5th, 2022</p>
-            <div class="card-actions mt-2">
-              <a
-                href="https://www.arrivelogistics.com/"
-                class="btn btn-outline btn-primary"
-                >Learn More<svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="-6 -6.5 24 24"
-                  width="21"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M7.828 2.414H2.243a1 1 0 1 1 0-2h8a.997.997 0 0 1 1 1v8a1 1 0 0 1-2 0V3.828l-6.779 6.779A1 1 0 0 1 1.05 9.192l6.778-6.778z"
-                  ></path></svg
-              ></a>
-            </div>
-          </div>
-        </div>
+        </div> -->
       </div>
     </div>
     <Footer />
