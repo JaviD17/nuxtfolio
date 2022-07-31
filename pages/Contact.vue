1ae4d881251b0adee4c254dd57363809eabf4812
Javier Dominguez II <Javi7057@gmail.com>
2022-07-31 13:24:29 -0500

    added outer div to template for contact page

--- pages/Contact.vue
+++ pages/Contact.vue
@@ -1,21 +1,27 @@
 <template>
-  <div class="hero min-h-screen bg-base-200">
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <div class="mb-8"><h1 class="title">Contact</h1></div>
-        <div
-          tabindex="0"
-          class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
-        >
-          <div class="collapse-title text-xl font-medium">
-            Focus me to see content
-          </div>
-          <div class="collapse-content">
-            <p>tabindex="0" attribute is necessary to make the div focusable</p>
+  <div>
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <div class="mb-8"><h1 class="title">Contact</h1></div>
+          <div
+            tabindex="0"
+            class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
+          >
+            <div class="collapse-title text-xl font-medium">
+              Focus me to see content
+            </div>
+            <div class="collapse-content">
+              <p>
+                tabindex="0" attribute is necessary to make the div focusable
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    <Footer />
   </div>
-  <Footer />
 </template>
+
+<style></style>
